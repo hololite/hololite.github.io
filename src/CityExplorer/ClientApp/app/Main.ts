65efe6507c5afcf794bf553a00f21888f6d3fcf9
PURESOUL\ahalim <antony@vizualkei.com>
2018-03-23 18:04:52 -0700

    modularized into Mapbox.ts

--- src/CityExplorer/ClientApp/app/Main.ts
+++ src/CityExplorer/ClientApp/app/Main.ts
@@ -1,14 +1,7 @@
-import * as mapboxgl from 'mapbox-gl';
+import { Mapbox } from './Mapbox';
 
 export class VkMap {
     static start(): void {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaG9sb2xpdGUiLCJhIjoiY2pleG9hdDlvMTh6ODJ6bjdpYm5mOTAycCJ9.3QrtCpe4LiSXtAuqV2FM1Q';
-
-        let map = new mapboxgl.Map({
-            container: 'map', // container id
-            style: 'mapbox://styles/mapbox/satellite-streets-v9', // stylesheet location
-            center: [-121.96, 37.35], // starting position [lng, lat]
-            zoom: 17 // starting zoom
-        });
+        Mapbox.start();
     }
 }
---
+++ src/CityExplorer/ClientApp/app/Mapbox.ts
@@ -0,0 +1,14 @@
+import * as mapboxgl from 'mapbox-gl';
+
+export class Mapbox {
+    static start(): void {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaG9sb2xpdGUiLCJhIjoiY2pleG9hdDlvMTh6ODJ6bjdpYm5mOTAycCJ9.3QrtCpe4LiSXtAuqV2FM1Q';
+
+        let map = new mapboxgl.Map({
+            container: 'map', // container id
+            style: 'mapbox://styles/mapbox/satellite-streets-v9', // stylesheet location
+            center: [-121.96, 37.35], // starting position [lng, lat]
+            zoom: 17 // starting zoom
+        });
+    }
+}
